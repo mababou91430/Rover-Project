d7c31b847e621b15c16eaf9da6212a919d8716f4
mathm <mathieu.dilhan91@gmail.Com>
2024-11-13 09:53:18 +0100

    RandMove

--- Tree.c
+++ Tree.c
@@ -28,44 +28,31 @@ t_node createTree_nr(t_map map, t_localisation loc_init){
         Tree->sons[i]->sons = malloc(8 * sizeof(t_node*));
         for(int j=0; j<8;j++){
             Tree->sons[i]->sons[j] = malloc(sizeof(t_node));
-            //randMove(loc_init);
+            randMove(loc_init);
             if(isValidLocalisation(loc_init.pos,7,6)){
                 Tree->sons[i]->sons[j]->value = map.costs[loc_init.pos.x][loc_init.pos.y];
             }
-//            else{
-//                printf("Position non valide");
-//            }
             Tree->sons[i]->sons[j]->sons = malloc(7*sizeof(t_node*));
             for(int k=0; k<7;k++){
                 Tree->sons[i]->sons[j]->sons[k] = malloc(sizeof(t_node));
-                //randMove(loc_init);
+                randMove(loc_init);
                 if(isValidLocalisation(loc_init.pos,7,6)){
                     Tree->sons[i]->sons[j]->sons[k]->value = map.costs[loc_init.pos.x][loc_init.pos.y];
                 }
-//                else{
-//                    printf("Position non valide");
-//                }
                 Tree->sons[i]->sons[j]->sons[k]->sons = malloc(6*sizeof(t_node*));
                 for (int l = 0; l < 6; l++) {
                     Tree->sons[i]->sons[j]->sons[k]->sons[l] = malloc(sizeof(t_node));
-                    //randMove(loc_init);
+                    randMove(loc_init);
                     if(isValidLocalisation(loc_init.pos,7,6)){
                         Tree->sons[i]->sons[j]->sons[k]->sons[l]->value = map.costs[loc_init.pos.x][loc_init.pos.y];
                     }
-//                    else {
-//                        printf("%d,%d\n",loc_init.pos.x,loc_init.pos.y);
-//                        printf("Position non valide\n");
-//                    }
                     Tree->sons[i]->sons[j]->sons[k]->sons[l]->sons = malloc(5*sizeof(t_node*));
                     for (int m = 0; m < 5; m++) {
                         Tree->sons[i]->sons[j]->sons[k]->sons[l]->sons[m] = malloc(sizeof(t_node));
-                        //randMove(loc_init);
+                        randMove(loc_init);
                         if(isValidLocalisation(loc_init.pos,7,6)){
                             Tree->sons[i]->sons[j]->sons[k]->sons[l]->sons[m]->value = map.costs[loc_init.pos.x][loc_init.pos.y];
                         }
-//                        else{
-//                            printf("Position non valide");
-//                        }
                         Tree->sons[i]->sons[j]->sons[k]->sons[l]->sons[m]->sons = NULL;
                    }
                 }
